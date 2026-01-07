062b7138a4a38a8b4c945cfd57aa9732de3a4c48
Tanmay Munjal <tanmaymunjal64@gmail.com>
2026-01-06 18:46:01 -0700

    lab done

---
+++ pet_shop/app/src/main/java/com/example/petshop/Happy.java
@@ -0,0 +1,18 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public class Happy extends Mood{
+    public Happy(){
+      super();
+    };
+
+    public Happy(Date date){
+        super(date);
+    };
+
+    @Override
+    public String moodString(){
+        return "happy";
+    }
+}
---
+++ pet_shop/app/src/main/java/com/example/petshop/Mood.java
@@ -0,0 +1,25 @@
+package com.example.petshop;
+
+import java.util.Date;
+
+public abstract class Mood {
+    private Date date;
+
+    public Mood(){
+      this.date = new Date();
+    };
+
+    public Mood(Date date){
+      this.date = date;
+    };
+
+    public Date getDate(){
+        return this.date;
+    };
+
+    public void setDate(Date date){
+        this.date = date;
+    };
+
+    public abstract String moodString();
+}
